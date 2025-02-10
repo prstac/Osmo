bde548f1c07fa32144c6232673347c7257c73b75
josip <josip@laptop>
2025-02-10 19:58:31 +0100

    trimmaj niz

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -4,7 +4,7 @@ public class Palindrom {
     }
 
     public static boolean isPalindrome(String nizZnakova) {
-        nizZnakova = nizZnakova.toLowerCase();
+        nizZnakova = nizZnakova.toLowerCase().trim();
         nizZnakova = String.join( "", nizZnakova.split(" "));
         for (int i = 0; i < nizZnakova.length()/2 ; i++) {
             if (nizZnakova.charAt(i) != nizZnakova.charAt(nizZnakova.length() - 1 - i)) {
